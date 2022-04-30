0586e795afd9ad6cbb952684ab5a8a5c0830c88a
faumui <ioane.faumui@queroquitar.com.br>
2022-04-30 16:35:23 -0300

    add menu for development

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,7 @@ import HomePage from "../components/HomePages/homepage";
 export default function Home() {
   return (
     <>
-      <button onClick={() => Router.push("/cadastro_servicos")}>
-        Cadastrar serviços
-      </button>
-      <button onClick={() => Router.push("/cadastro_products")}>
-        Cadastrar Produtos
-      </button>
+      <HomePage />
     </>
   );
 }
