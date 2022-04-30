a95d4ba21cc7489ad94dd71d82e6c3332b9a4732
faumui <ioane.faumui@queroquitar.com.br>
2022-04-30 17:50:25 -0300

    save progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,36 @@
 import "./_app.css";
 import GlobalStyle from "../styles/GlobalStyle";
 import Router from "next/router";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
-      <div
-        style={{
-          position: "absolute",
-          right: "0",
-          display: "flex",
-          flexDirection: "column",
-          width: "200px",
-          fontSize: "13px",
-        }}
-      >
-        <button onClick={() => Router.push("/cadastro_servicos")}>
-          Cadastrar serviços
-        </button>
-        <button onClick={() => Router.push("/cadastro_products")}>
-          Cadastrar Produtos
-        </button>
-        <button onClick={() => Router.push("/landing-page")}>
-          Landing page
-        </button>
-        Esse menu é somente pra facilitar a navegação em desenvolvimento
-      </div>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <SessionProvider>
+        <div
+          style={{
+            position: "absolute",
+            right: "0",
+            display: "flex",
+            flexDirection: "column",
+            width: "200px",
+            fontSize: "13px",
+          }}
+        >
+          <button onClick={() => Router.push("/cadastro_servicos")}>
+            Cadastrar serviços
+          </button>
+          <button onClick={() => Router.push("/cadastro_products")}>
+            Cadastrar Produtos
+          </button>
+          <button onClick={() => Router.push("/landing-page")}>
+            Landing page
+          </button>
+          Esse menu é somente pra facilitar a navegação em desenvolvimento
+        </div>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
