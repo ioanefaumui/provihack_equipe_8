7f999ad99fad206ef0115ee18883e89d58cf9e10
Ayana <ayanamello27@gmail.com>
2022-05-01 00:12:24 -0300

    feat: update btnOptions component

--- components/BtnOptions/styles.js
+++ components/BtnOptions/styles.js
@@ -1,17 +1,31 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
-  div {
-    width: 12%;
-    height: 24%;
-    position: relative;
-    margin: 5% auto;
-  }
-  width: 12vw;
-  height: 11vh;
+export const Content = styled.div`
+  width: 10vw;
+  height: 12vh;
   border: none;
   border-radius: 10px;
-  text-align: center;
-  font-size: 14px;
-  background-color: ${(props) => props.backgroundColor};
+  position: relative;
+  background-image: url(${props => props.background});
+
+  div {
+    width: 100%;
+    background-image: linear-gradient(to top, #157D7E, rgba(21, 125,126, 0));
+    height: 8vh;
+    border-radius: 10px;
+    position: absolute;
+    bottom: 0;
+    padding-top: 2em;
+  }
+
+  div p {
+    width: 60%;
+    color: #FFF;
+    text-align: center;
+    font-weight: 600;
+    font-size: 15px;
+    margin: 0 auto;
+    line-height: 1.2;
+    text-transform: uppercase;
+  }
 `;
